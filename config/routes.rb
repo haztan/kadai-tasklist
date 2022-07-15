9c9acf2cbecaff1f1302dd2d62d57b8411260810
haztan <haztandx0508@gmail.com>
2022-07-15 07:38:34 +0000

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   root to: "tasks#index"
+  
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
   resources :tasks
+  resources :users, only: [:create] 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
